a1459a1ef05b1bcbb4a5edf728f50c3440c9117d
sylim <sylim@danbiedu.co.kr>
2024-06-08 16:27:37 +0900

    [MOD] 강연 시작/종료 시간 패턴 설정

--- task-back/src/main/java/com/task/back/dto/LectureDto.java
+++ task-back/src/main/java/com/task/back/dto/LectureDto.java
@@ -1,8 +1,10 @@
 package com.task.back.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -34,11 +36,16 @@ public class LectureDto {
         @NotNull
         private Integer totalCount;
 
-        @Schema(description = "강연 시작 시간")
+        @Schema(description = "강연 시작 시간", example = "2024-06-15 09:00:00")
         @NotEmpty
+        @Pattern(regexp = "\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}",
+                message = "날짜 형식은 다음과 같아야 합니다: yyyy-MM-dd HH:mm:ss")
         private String lectureStartTime;
 
-        @Schema(description = "강연 종료 시간")
+        @Schema(description = "강연 종료 시간", example = "2024-06-15 18:00:00")
+        @NotEmpty
+        @Pattern(regexp = "\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}",
+                message = "날짜 형식은 다음과 같아야 합니다: yyyy-MM-dd HH:mm:ss")
         private String lectureEndTime;
 
         @Schema(description = "강연 내용")
